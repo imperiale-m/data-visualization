12a216cfc8b51032d2456d50787f87fd88da9dae
imperiale-x <57863485+imperiale-x@users.noreply.github.com>
2022-11-04 19:36:41 +0100

    format code

--- asset/task1.js
+++ asset/task1.js
@@ -11,7 +11,8 @@ width = WIDTH - margin.left - margin.right;
 height = HEIGHT - margin.top - margin.bottom;
 
 // append the svg object to the div with id #task1
-const svg = d3.select("#task1")
+const svg = d3
+  .select("#task1")
   .append("svg")
   .attr("width", width + margin.left + margin.right)
   .attr("height", height + margin.top + margin.bottom)
@@ -19,8 +20,7 @@ const svg = d3.select("#task1")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 // Data
-d3.csv("../data/task1.csv", d3.autoType).then(function(data) {
-
+d3.csv("../data/task1.csv", d3.autoType).then(function (data) {
   // number of elements to plot
   const N = 20;
 
@@ -31,27 +31,30 @@ d3.csv("../data/task1.csv", d3.autoType).then(function(data) {
   // max value for the x-axis
   const max = d3.max(data, d => d.count);
 
-  const x = d3.scaleLinear()
+  const x = d3
+    .scaleLinear()
     .range([0, width * 0.95])
     .domain([0, max]);
 
-  const y = d3.scaleBand()
+  const y = d3
+    .scaleBand()
     .range([0, height])
     .domain(data.map(d => d.name))
-    .padding(.1);
+    .padding(0.1);
 
   // plot the x-axis
-  svg.append("g")
+  svg
+    .append("g")
     .attr("transform", `translate(0, ${height})`)
     .call(d3.axisBottom(x))
     .selectAll("text")
     .style("text-anchor", "end");
 
   // plot the y-axis
-  svg.append("g")
-    .call(d3.axisLeft(y));
+  svg.append("g").call(d3.axisLeft(y));
 
-  const tooltip = d3.select("#task1")
+  const tooltip = d3
+    .select("#task1")
     .append("div")
     .style("opacity", 0)
     .attr("class", "tooltip")
@@ -62,30 +65,25 @@ d3.csv("../data/task1.csv", d3.autoType).then(function(data) {
     .style("border-radius", "5px")
     .style("padding", "8px");
 
-  const mouseover = function() {
-    tooltip
-      .style("opacity", 1);
-    d3.select(this)
-      .style("stroke", "black")
-      .style("opacity", 1);
+  const mouseover = function () {
+    tooltip.style("opacity", 1);
+    d3.select(this).style("stroke", "black").style("opacity", 1);
   };
 
-  const mouseout = function() {
-    tooltip
-      .style("opacity", 0);
-    d3.select(this)
-      .style("stroke", "none")
-      .style("opacity", 0.8);
+  const mouseout = function () {
+    tooltip.style("opacity", 0);
+    d3.select(this).style("stroke", "none").style("opacity", 0.8);
   };
 
-  const mousemove = function(event, d) {
+  const mousemove = function (event, d) {
     tooltip
-      .html("The mean canopy size is: " + d['mean_canopy_size'])
-      .style("top", (event.pageY) + "px")
-      .style("left", (event.pageX + 20) + "px");
+      .html("The mean canopy size is: " + d["mean_canopy_size"])
+      .style("top", event.pageY + "px")
+      .style("left", event.pageX + 20 + "px");
   };
 
-  svg.selectAll("rect")
+  svg
+    .selectAll("rect")
     .data(data)
     .join("rect")
     .attr("x", 0)
