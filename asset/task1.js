533e4c9aa7c830b4cffc8b02a98a70f93a4b8368
imperiale-x <57863485+imperiale-x@users.noreply.github.com>
2022-11-04 19:52:16 +0100

    add catch

--- asset/task1.js
+++ asset/task1.js
@@ -20,78 +20,82 @@ const svg = d3
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 // Data
-d3.csv("../data/task1.csv", d3.autoType).then(function (data) {
-  // number of elements to plot
-  const N = 20;
+ad3.csv("../data/task1.csv", d3.autoType)
+  .then(function (data) {
+    // number of elements to plot
+    const N = 20;
 
-  data = data.slice(0, N);
+    data = data.slice(0, N);
 
-  // console.log(data);
+    // console.log(data);
 
-  // max value for the x-axis
-  const max = d3.max(data, d => d.count);
+    // max value for the x-axis
+    const max = d3.max(data, d => d.count);
 
-  const x = d3
-    .scaleLinear()
-    .range([0, width * 0.95])
-    .domain([0, max]);
+    const x = d3
+      .scaleLinear()
+      .range([0, width * 0.95])
+      .domain([0, max]);
 
-  const y = d3
-    .scaleBand()
-    .range([0, height])
-    .domain(data.map(d => d.name))
-    .padding(0.1);
+    const y = d3
+      .scaleBand()
+      .range([0, height])
+      .domain(data.map(d => d.name))
+      .padding(0.1);
 
-  // plot the x-axis
-  svg
-    .append("g")
-    .attr("transform", `translate(0, ${height})`)
-    .call(d3.axisBottom(x))
-    .selectAll("text")
-    .style("text-anchor", "end");
+    // plot the x-axis
+    svg
+      .append("g")
+      .attr("transform", `translate(0, ${height})`)
+      .call(d3.axisBottom(x))
+      .selectAll("text")
+      .style("text-anchor", "end");
 
-  // plot the y-axis
-  svg.append("g").call(d3.axisLeft(y));
+    // plot the y-axis
+    svg.append("g").call(d3.axisLeft(y));
 
-  const tooltip = d3
-    .select("#task1")
-    .append("div")
-    .style("opacity", 0)
-    .attr("class", "tooltip")
-    .style("background-color", "white")
-    .style("position", "absolute")
-    .style("border", "solid")
-    .style("border-width", "1px")
-    .style("border-radius", "5px")
-    .style("padding", "8px");
+    const tooltip = d3
+      .select("#task1")
+      .append("div")
+      .style("opacity", 0)
+      .attr("class", "tooltip")
+      .style("background-color", "white")
+      .style("position", "absolute")
+      .style("border", "solid")
+      .style("border-width", "1px")
+      .style("border-radius", "5px")
+      .style("padding", "8px");
 
-  const mouseover = function () {
-    tooltip.style("opacity", 1);
-    d3.select(this).style("stroke", "black").style("opacity", 1);
-  };
+    const mouseover = function () {
+      tooltip.style("opacity", 1);
+      d3.select(this).style("stroke", "black").style("opacity", 1);
+    };
 
-  const mouseout = function () {
-    tooltip.style("opacity", 0);
-    d3.select(this).style("stroke", "none").style("opacity", 0.8);
-  };
+    const mouseout = function () {
+      tooltip.style("opacity", 0);
+      d3.select(this).style("stroke", "none").style("opacity", 0.8);
+    };
 
-  const mousemove = function (event, d) {
-    tooltip
-      .html("The mean canopy size is: " + d["mean_canopy_size"])
-      .style("top", event.pageY + "px")
-      .style("left", event.pageX + 20 + "px");
-  };
+    const mousemove = function (event, d) {
+      tooltip
+        .html("The mean canopy size is: " + d["mean_canopy_size"])
+        .style("top", event.pageY + "px")
+        .style("left", event.pageX + 20 + "px");
+    };
 
-  svg
-    .selectAll("rect")
-    .data(data)
-    .join("rect")
-    .attr("x", 0)
-    .attr("y", d => y(d.name))
-    .attr("width", d => x(d.count))
-    .attr("height", y.bandwidth())
-    .attr("fill", "orange")
-    .on("mouseover", mouseover)
-    .on("mouseout", mouseout)
-    .on("mousemove", mousemove);
-});
+    svg
+      .selectAll("rect")
+      .data(data)
+      .join("rect")
+      .attr("x", 0)
+      .attr("y", d => y(d.name))
+      .attr("width", d => x(d.count))
+      .attr("height", y.bandwidth())
+      .attr("fill", "orange")
+      .on("mouseover", mouseover)
+      .on("mouseout", mouseout)
+      .on("mousemove", mousemove);
+  })
+  .catch(e => {
+    console.log(e);
+  });
