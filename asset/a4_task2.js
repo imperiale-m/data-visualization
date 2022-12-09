64edbf19a22201cc622a98336dc636e6494843a0
imperiale-x <57863485+imperiale-x@users.noreply.github.com>
2022-12-09 17:45:19 +0100

    task2 completed

--- asset/a4_task2.js
+++ asset/a4_task2.js
@@ -11,7 +11,7 @@ d3.csv('../data/assignment4.csv', d3.autoType)
     const height = 400;
 
     const group = d3.flatGroup(data, (d) => d.year);
-    // Add the fisrt element to the end for each year
+    // Add the first element to the end for each year
     group.map((d) => d[1].push(d[1][0]));
     const sumstat = [...group.values()];
 
@@ -22,7 +22,6 @@ d3.csv('../data/assignment4.csv', d3.autoType)
       .attr('viewBox', [0, 0, width + margin.l + margin.r, height + margin.t + margin.b])
       .attr('style', 'max-width: 100%; height: auto')
       .append('g')
-      // .attr('transform', `translate(${margin.l}, ${margin.t})`);
       .attr('transform', `translate(${width / 2 + margin.l},${height / 2 + margin.t})`);
 
     const months = [
@@ -41,11 +40,9 @@ d3.csv('../data/assignment4.csv', d3.autoType)
     ];
 
     const dom = d3.extent(data, (d) => d.mean);
-
-    // const allAxis = data[0].map((i, j) => i.axis); //Names of each axis
     const total = months.length; // The number of different axes
     const radius = Math.min(width / 2, height / 2); // Radius of the outermost circle
-    const angleSlice = (Math.PI * 2) / total; // The width in radians of each "slice"
+    const angleSlice = (Math.PI * 2) / total; // The width in radians of each 'slice'
 
     // Scale for the radius
     const rScale = d3.scaleLinear().range([0, radius]).domain(dom);
@@ -55,6 +52,60 @@ d3.csv('../data/assignment4.csv', d3.autoType)
 
     const levels = 5;
 
+    const aux = data.map((d) => d.year);
+    const years = [...new Set(aux)];
+
+    const yearsDict = d3.rollup(
+      years,
+      () => true,
+      (d) => d,
+    );
+
+    const tooltip2 = d3.select('#a4_task2').append('div').attr('class', 'tooltip');
+
+    function mouseover() {
+      tooltip2.style('z-index', 1);
+      tooltip2.transition().style('opacity', 0.9);
+      d3.select(this).transition().attr('r', 6);
+    }
+
+    function mouseout() {
+      tooltip2.style('z-index', -1);
+      tooltip2.transition().style('opacity', 0);
+      d3.select(this).transition().attr('r', 4);
+    }
+
+    function mousemove(event, d) {
+      let text = '';
+      switch (this.className.baseVal) {
+        case 'dot dotMin':
+          text = `Min: ${d.min}&degC`;
+          break;
+        case 'dot dotMax':
+          text = `Max: ${d.max}&degC`;
+          break;
+        case 'dot dotMean':
+          text = `Mean: ${d.mean}&degC`;
+          break;
+        default:
+          text = 'Error';
+      }
+      tooltip2
+        .html(`<b>${months[d.month - 1]} ${d.year}</b><br>${text}`)
+        .style('top', `${event.pageY}px`)
+        .style('left', `${event.pageX + 20}px`);
+    }
+
+    // The radial line function
+    const radarLine = d3
+      .lineRadial()
+      .radius((d) => rScale(d.mean))
+      .angle((d, i) => i * angleSlice);
+
+    const colors = d3.scaleOrdinal(years, d3.schemeTableau10);
+
+    const legend = svg.append('g').attr('id', 'legend2').selectAll('.legend2').data(years);
+
     // Draw the background circles
     axisGrid
       .selectAll('.levels')
@@ -62,12 +113,11 @@ d3.csv('../data/assignment4.csv', d3.autoType)
       .join('circle')
       .attr('class', 'gridCircle')
       .attr('r', (d) => (radius / levels) * d)
-      .style('fill', '#CDCDCD')
-      .style('stroke', '#CDCDCD')
+      .style('fill', '#D3D3D3')
+      .style('stroke', '#DCDCDC')
       .style('fill-opacity', 0.1);
-    // .style('filter', 'url(#glow)');
 
-    // Text indicating at what % each level is
+    // Text indicating at what each level is
     axisGrid
       .selectAll('.axisLabel')
       .data(d3.range(1, levels + 1).reverse())
@@ -77,7 +127,7 @@ d3.csv('../data/assignment4.csv', d3.autoType)
       .attr('y', (d) => (-d * radius) / levels)
       .attr('dy', '0.4em')
       .style('font-size', '10px')
-      .attr('fill', '#737373')
+      .attr('fill', 'gray')
       .text((d) => `${Math.round((dom[1] * d) / levels)}°C`);
 
     // Create the straight lines radiating outward from the center
@@ -89,8 +139,8 @@ d3.csv('../data/assignment4.csv', d3.autoType)
       .attr('y1', 0)
       .attr('x2', (d, i) => rScale(dom[1] * 1.1) * Math.cos(angleSlice * i - Math.PI / 2))
       .attr('y2', (d, i) => rScale(dom[1] * 1.1) * Math.sin(angleSlice * i - Math.PI / 2))
-      .attr('class', 'line')
-      .style('stroke', 'gray')
+      .attr('class', 'line2')
+      .style('stroke', '#D3D3D3')
       .style('stroke-width', '1px');
 
     // Append the labels at each axis
@@ -100,245 +150,116 @@ d3.csv('../data/assignment4.csv', d3.autoType)
       .style('font-size', '11px')
       .attr('text-anchor', 'middle')
       .attr('dy', '0.35em')
-      .attr('x', (d, i) => rScale(dom[1] * 1.25) * Math.cos(angleSlice * i - Math.PI / 2))
-      .attr('y', (d, i) => rScale(dom[1] * 1.25) * Math.sin(angleSlice * i - Math.PI / 2))
+      .attr('x', (d, i) => rScale(dom[1] * 1.2) * Math.cos(angleSlice * i - Math.PI / 2))
+      .attr('y', (d, i) => rScale(dom[1] * 1.2) * Math.sin(angleSlice * i - Math.PI / 2))
       .text((d) => d);
-    // .call(wrap, 60);
 
-    // The radial line function
-    const radarLine = d3
-      .lineRadial()
-      // .interpolate('linear-closed')
-      .radius((d) => rScale(d.mean))
-      .angle((d, i) => i * angleSlice);
+    function redrawChart() {
+      svg.selectAll('.radarWrapper').remove();
+      const filteredData = sumstat.filter((yr) => yearsDict.get(yr[0]));
 
-    const aux = data.map((d) => d.year);
-    const years = [...new Set(aux)];
-    // Create a wrapper for the blobs
-    const blobWrapper = svg
-      .selectAll('.radarWrapper')
-      .data(sumstat)
-      .join('g')
-      .attr('class', 'radarWrapper');
-    const colors = d3.scaleOrdinal().domain(years).range(d3.schemeTableau10);
-    // Append the backgrounds
-    blobWrapper
-      .append('path')
-      .attr('class', 'radarArea')
-      .attr('d', (d) => radarLine(d[1]))
-      .style('fill', (d) => colors(d[0]))
-      .style('fill-opacity', 0.1);
-    // .on('mouseover', function (d, i) {
-    //   // Dim all blobs
-    //   d3.selectAll('.radarArea').transition().duration(200).style('fill-opacity', 0.1);
-    //   // Bring back the hovered over blob
-    //   d3.select(this).transition().duration(200).style('fill-opacity', 0.7);
-    // })
-    // .on('mouseout', () => {
-    //   // Bring back all blobs
-    //   d3.selectAll('.radarArea')
-    //     .transition()
-    //     .duration(200)
-    //     .style('fill-opacity', cfg.opacityArea);
-    // });
+      // Create a wrapper for the blobs
+      const blobWrapper = svg
+        .selectAll('.radarWrapper')
+        .data(filteredData)
+        .join('g')
+        .attr('class', 'radarWrapper');
 
-    // Create the outlines
-    blobWrapper
-      .append('path')
-      .attr('class', 'radarStroke')
-      .attr('d', (d) => radarLine(d[1]))
-      .style('stroke-width', '1px')
-      .style('stroke', (d) => colors(d[0]))
-      .style('fill', 'none');
-    // .style('filter', 'url(#glow)');
+      // Append the backgrounds
+      blobWrapper
+        .append('path')
+        .attr('class', 'radarArea')
+        .attr('d', (d) => radarLine(d[1]))
+        .style('fill', (d) => colors(d[0]))
+        .style('fill-opacity', 0.1);
 
-    // Append the circles
-    blobWrapper
-      .selectAll('.radarCircle')
-      .data((d) => d[1])
-      .enter()
-      .append('circle')
-      .attr('class', 'radarCircle')
-      .attr('r', 3)
-      .attr('cx', (d, i) => rScale(d.mean) * Math.cos(angleSlice * i - Math.PI / 2))
-      .attr('cy', (d, i) => rScale(d.mean) * Math.sin(angleSlice * i - Math.PI / 2))
-      .style('fill', (d) => colors(d.year))
-      .style('fill-opacity', 0.8);
+      // Create the outlines
+      blobWrapper
+        .append('path')
+        .attr('class', 'radarStroke')
+        .attr('d', (d) => radarLine(d[1]))
+        .style('stroke-width', '1px')
+        .style('stroke', (d) => colors(d[0]))
+        .style('fill', 'none');
 
-    // const yearsDict = d3.rollup(
-    //   years,
-    //   () => true,
-    //   (d) => d,
-    // );
+      // Append the circles
+      blobWrapper
+        .selectAll('.radarCircle')
+        .data((d) => d[1])
+        .join('circle')
+        .attr('class', 'radarCircle')
+        .attr('r', 3)
+        .attr('cx', (d, i) => rScale(d.mean) * Math.cos(angleSlice * i - Math.PI / 2))
+        .attr('cy', (d, i) => rScale(d.mean) * Math.sin(angleSlice * i - Math.PI / 2))
+        .style('fill', (d) => colors(d.year))
+        .style('fill-opacity', 1)
+        .style('stroke', 'none')
+
+      const test = d3.selectAll('.textSelected2');
+      const rect = d3.selectAll('.check2');
+      function toggle(element) {
+        d3.select(this).classed('disabled', !yearsDict.get(element));
+      }
+      test.each(toggle);
+      rect.each(toggle);
+    }
+    redrawChart();
     //
-    // const tooltip = d3.select('#a4_task1').append('div').attr('class', 'tooltip');
-    //
-    // function mouseover() {
-    //   tooltip.style('z-index', 1);
-    //   tooltip.transition().style('opacity', 0.9);
-    //   d3.select(this).transition().attr('r', 6);
-    // }
-    //
-    // function mouseout() {
-    //   tooltip.style('z-index', -1);
-    //   tooltip.transition().style('opacity', 0);
-    //   d3.select(this).transition().attr('r', 4);
-    // }
-    //
-    // function mousemove(event, d) {
-    //   let text = '';
-    //   switch (this.className.baseVal) {
-    //     case 'dot dotMin':
-    //       text = `Min: ${d.min}&degC`;
-    //       break;
-    //     case 'dot dotMax':
-    //       text = `Max: ${d.max}&degC`;
-    //       break;
-    //     case 'dot dotMean':
-    //       text = `Mean: ${d.mean}&degC`;
-    //       break;
-    //     default:
-    //       text = 'Error';
-    //   }
-    //   tooltip
-    //     .html(`<b>${months[d.month - 1]} ${d.year}</b><br>${text}`)
-    //     .style('top', `${event.pageY}px`)
-    //     .style('left', `${event.pageX + 20}px`);
-    // }
-    //
-    // const legend = svg.append('g').attr('class', 'legends').selectAll('.legend').data(years);
-    // function redrawChart() {
-    //   svg.selectAll('.line').remove();
-    //   svg.selectAll('.dot').remove();
-    //
-    //   const filteredData = sumstat.filter((yr) => yearsDict.get(yr[0]));
-    //
-    //   svg
-    //     .selectAll('.lineMin')
-    //     .data(filteredData)
-    //     .join('path')
-    //     .attr('class', 'line')
-    //     .attr('d', (d) => lineMin(d[1]))
-    //     .attr('stroke', (d) => lighten(colors(d[0])))
-    //     .style('stroke-width', 1)
-    //     .style('fill', 'none');
-    //
-    //   svg
-    //     .selectAll('.dotMin')
-    //     .data(data.filter((d) => yearsDict.get(d.year)))
-    //     .join('circle')
-    //     .attr('class', 'dot dotMin')
-    //     .attr('cx', lineMin.x())
-    //     .attr('cy', lineMin.y())
-    //     .attr('r', 3.5)
-    //     .style('fill', (d) => lighten(colors(d.year)))
-    //     .style('stroke', 'white')
-    //     .style('stroke-width', '1px');
-    //
-    //   svg
-    //     .selectAll('.lineMax')
-    //     .data(filteredData)
-    //     .join('path')
-    //     .attr('class', 'line')
-    //     .attr('d', (d) => lineMax(d[1]))
-    //     .attr('stroke', (d) => darken(colors(d[0])))
-    //     .style('stroke-width', 1)
-    //     .style('fill', 'none');
-    //
-    //   svg
-    //     .selectAll('.dotMax')
-    //     .data(data.filter((d) => yearsDict.get(d.year)))
-    //     .join('circle')
-    //     .attr('class', 'dot dotMax')
-    //     .attr('cx', lineMax.x())
-    //     .attr('cy', lineMax.y())
-    //     .attr('r', 3.5)
-    //     .style('fill', (d) => darken(colors(d.year)))
-    //     .style('stroke', 'white')
-    //     .style('stroke-width', '1px');
-    //
-    //   svg
-    //     .selectAll('.dotMean')
-    //     .data(data.filter((d) => yearsDict.get(d.year)))
-    //     .join('circle')
-    //     .attr('class', 'dot dotMean')
-    //     .attr('cx', (d) => x(months[d.month - 1]))
-    //     .attr('cy', (d) => y(d.mean))
-    //     .attr('r', 3)
-    //     .style('fill', (d) => colors(d.year))
-    //     .style('stroke', 'none')
-    //     .style('stroke-width', '0.5px');
-    //
-    //   svg
-    //     .selectAll('.dot')
-    //     .on('mouseover', mouseover)
-    //     .on('mouseout', mouseout)
-    //     .on('mousemove', mousemove);
-    //
-    //   const test = d3.selectAll('.textSelected');
-    //   const rect = d3.selectAll('rect');
-    //   function toggle(element) {
-    //     d3.select(this).classed('disabled', !yearsDict.get(element));
-    //   }
-    //   test.each(toggle);
-    //   rect.each(toggle);
-    // }
-    // redrawChart();
-    //
-    // // LEGEND
-    // function clickLegendHandler(event, yr) {
-    //   yearsDict.set(yr, !yearsDict.get(yr));
-    //   redrawChart();
-    // }
-    //
-    // legend
-    //   .join('rect')
-    //   .attr('x', width + margin.r - 80)
-    //   .attr('y', (d, i) => 20 * i)
-    //   .attr('width', 12)
-    //   .attr('height', 12)
-    //   .attr('class', 'check')
-    //   .style('fill', (d) => colors(d))
-    //   .style('cursor', 'pointer')
-    //   .on('click', clickLegendHandler);
-    // legend
-    //   .join('text')
-    //   .attr('x', width + margin.r - 60)
-    //   .attr('y', (d, i) => 20 * i + 7)
-    //   .text((d) => d)
-    //   .attr('class', 'textSelected')
-    //   .style('font-size', '12px')
-    //   .style('alignment-baseline', 'middle');
-    //
-    // const l = d3.select('.legends');
-    // l.append('text')
-    //   .attr('x', width + margin.r - 80)
-    //   .attr('y', 20 * 8 + 12)
-    //   .attr('class', 'hide-all-option underline hover:no-underline')
-    //   .style('fill', 'steelblue')
-    //   .style('cursor', 'pointer')
-    //   .text('hide all')
-    //   .style('font-size', '11px')
-    //   .on('click', () => {
-    //     years.forEach((yr) => {
-    //       yearsDict.set(yr, false);
-    //     });
-    //     redrawChart();
-    //   });
-    // l.append('text')
-    //   .attr('x', width + margin.r - 40)
-    //   .attr('y', 20 * 8 + 12)
-    //   .attr('class', 'show-all-option underline hover:no-underline')
-    //   .style('fill', 'steelblue')
-    //   .style('cursor', 'pointer')
-    //   .text('show all')
-    //   .style('font-size', '11px')
-    //   .on('click', () => {
-    //     years.forEach((yr) => {
-    //       yearsDict.set(yr, true);
-    //     });
-    //     redrawChart();
-    //   });
+    // LEGEND
+    function clickLegendHandler(event, yr) {
+      yearsDict.set(yr, !yearsDict.get(yr));
+      redrawChart();
+    }
+
+    legend
+      .join('rect')
+      .attr('x', width / 2)
+      .attr('y', (d, i) => 20 * i - height / 2)
+      .attr('width', 12)
+      .attr('height', 12)
+      .attr('class', 'check2')
+      .style('fill', (d) => colors(d))
+      .style('cursor', 'pointer')
+      .on('click', clickLegendHandler);
+    legend
+      .join('text')
+      .attr('x', width / 2 + 20)
+      .attr('y', (d, i) => 20 * i - height / 2 + 7)
+      .text((d) => d)
+      .attr('class', 'textSelected2')
+      .style('font-size', '12px')
+      .style('alignment-baseline', 'middle');
+
+    const l = d3.select('#legend2');
+
+    l.append('text')
+      .attr('x', width / 2)
+      .attr('y', 20 * 8 - height / 2 + 12)
+      .attr('class', 'underline hover:no-underline')
+      .style('fill', 'steelblue')
+      .style('cursor', 'pointer')
+      .text('hide all')
+      .style('font-size', '11px')
+      .on('click', () => {
+        years.forEach((yr) => {
+          yearsDict.set(yr, false);
+        });
+        redrawChart();
+      });
+    l.append('text')
+      .attr('x', width / 2 + 40)
+      .attr('y', 20 * 8 - height / 2 + 12)
+      .attr('class', 'underline hover:no-underline')
+      .style('fill', 'steelblue')
+      .style('cursor', 'pointer')
+      .text('show all')
+      .style('font-size', '11px')
+      .on('click', () => {
+        years.forEach((yr) => {
+          yearsDict.set(yr, true);
+        });
+        redrawChart();
+      });
   })
   .catch((e) => {
     console.log(e);
